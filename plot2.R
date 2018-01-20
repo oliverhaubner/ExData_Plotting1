95f59d3b46ee92e3583b59b43f696e3e2b801ed7
oliverhaubner <35100486+oliverhaubner@users.noreply.github.com>
2018-01-20 13:26:21 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,22 @@
-#plot 2
+setwd("~/COURSERA/Exploratory/Week 1/powerconsumption")
+global <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+#set time format
+global$Date <- strptime(global$Date, format = "%d/%m/%Y")
+#subset to 1st and 2nd february 2007
+feb12 <- subset(global, global$Date == as.POSIXlt("1/2/2007", format = "%d/%m/%Y") | global$Date == as.POSIXlt("2/2/2007", format = "%d/%m/%Y"))
+#paste Date and Time columns
+feb12$Date <- paste(feb12$Date, feb12$Time)
+#set time format
+feb12$Date <- strptime(feb12$Date, format = "%Y-%m-%d %H:%M:%S")
+df <- data.frame(feb12)
+#set numeric vectors
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
 
+#plot 2
 png(file = "plot2.png")
     plot(df$Date, df$Global_active_power,  ylab = "Global Active Power (kilowatts)", 
     xlab = "", lty = 1, type = "l")
